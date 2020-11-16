edd0532fe43f88fd9da69d9b25420db592d74a77
David Holland <dave@dholland.net>
2020-11-15 18:55:31 -0700

    Add files via upload

--- Main.lua
+++ Main.lua
@@ -2,6 +2,34 @@ local _, PinHelper = ...;
 
 SLASH_MapPinHelper1 = '/pin';
 
+function MapPinHelper_HelpText()
+	print("Map Pin Helper Commands Function")
+	print("----------------------------")
+	print(" - Must supply coordinates (two numbers) for example: /pin 42 42")
+	print(" - Note that valid coordinates are between 0 - 100")
+	print(" - You can also clear pins with: /pin clear")
+	print("----------------------------")
+end
+
+function MapPinHelper_DoPin(options)
+	valid = false
+	if #options == 2 then
+		x=(options[1] / 100);
+		y=(options[2] / 100);
+		valid = true
+	end
+	if (valid and x<=1 and y<=1) then
+		map=C_Map;
+		player='player';
+		location=map.GetBestMapForUnit(player);
+		map.SetUserWaypoint(UiMapPoint.CreateFromCoordinates(location,x,y));
+		print("Creating new map pin at " .. options[1] .. " - " .. options[2])
+	else
+		MapPinHelper_HelpText()
+end
+
+end
+
 function SlashCmdList.MapPinHelper(msg, editbox)
 	if msg == "clear" then
 		C_Map.ClearUserWaypoint()
@@ -12,24 +40,10 @@ function SlashCmdList.MapPinHelper(msg, editbox)
 		for i,v in pairs(searchResult) do
         		if (v ~= nil and v ~= "") then
             		options[i] = v
-       		 end
-    	end
-
-	if #options == 2 then
-		x=(options[1] / 100);
-		y=(options[2] / 100);
+       			end
+    		end
+		MapPinHelper_DoPin(options)
 	
-		map=C_Map;
-		player='player';
-		location=map.GetBestMapForUnit(player);
-		map.SetUserWaypoint(UiMapPoint.CreateFromCoordinates(location,x,y));
-		print("Creating new map pin at " .. options[1] .. " - " .. options[2])
-	else
-		print("Map Pin Helper Commands")
-		print("----------------------------")
-		print(" - Must supply coordinates (two numbers) for example: /pin 42 42")
-		print(" - You can also clear pins with: /pin clear")
-		print("----------------------------")
-	end
 end
+
 end
